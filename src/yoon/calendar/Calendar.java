57cd124ad817d72c3133e7a8be3b7434f481ab83
17chas <17chas@naver.com>
2021-09-21 23:14:14 +0900

    두 수의 합 구하기 풀이

--- src/yoon/calendar/Calendar.java
+++ src/yoon/calendar/Calendar.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("두 수를 입력하세요.");
-		
+
+		int a, b;
+
 		Scanner scanner = new Scanner(System.in);
-		String inputValue = scanner.nextLine();
-		String[] splitedValue = inputValue.split(" ");
-		
-		int first = Integer.parseInt(splitedValue[0]);
-		int second = Integer.parseInt(splitedValue[1]);
-		
-		//int sum = first + second;
-		
-		System.out.println("두 수의 합은 "+ (first+second) +"입니다");
+
+		String s1, s2;
+
+		System.out.println("두 수를 입력해주세요.");
+
+		s1 = scanner.next();
+		s2 = scanner.next();
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+
+		System.out.printf("%d와 %d의 합은 %d입니다", a, b, a + b);
+		scanner.close();
+
 	}
 
 }
