f48c78d090422d5f0f10571a3c80add0eeadb4c1
17chas <17chas@naver.com>
2021-09-30 23:26:40 +0900

    n번 반복가능하도록 프로그램 변경하기 (실패)

--- src/yoon/calendar/Calendar1.java
+++ src/yoon/calendar/Calendar1.java
@@ -3,23 +3,28 @@ package yoon.calendar;
 import java.util.Scanner;
 
 public class Calendar1 {
-	
+
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int maxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	
-	
-	public static void main(String[] args) {
 
-		System.out.println("달을 입력하세요");
+	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 		Calendar1 cal = new Calendar1();
-		int month = scanner.nextInt();
-		scanner.close();
 
-		System.out.printf("%d월은 %d일까지 있습니다..\n", month, cal.maxDaysOfMonth(month));
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+
+		for (int i = 0;i < repeat; i++); {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxDaysOfMonth(month));
+		}
+
+		scanner.close();
 
 	}
 }
